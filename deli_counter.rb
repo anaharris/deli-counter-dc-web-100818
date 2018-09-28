3646c4cd6cb3f8077dac3a41ba55743f6cb8d415
Ana Harris <vana1990@gmail.com>
2018-09-28 23:35:39 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,13 +2,13 @@ def line (katz_deli)
   customers = []
   x = 1
   if katz_deli.size < 1
-    return "The line is currently empty."
+    puts "The line is currently empty."
   else 
     katz_deli.each do |i|
     customers << "#{x}. #{i} "
     x+= 1
     end
   end
-  return "The line is currently: #{customers.join}"
+  puts "The line is currently: #{customers.join}"
 end
 
