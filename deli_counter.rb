55ac6c7e10108c8a254ef6334c5af12c59415710
Ana Harris <vana1990@gmail.com>
2018-09-28 23:35:57 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line (katz_deli)
     puts "The line is currently empty."
   else 
     katz_deli.each do |i|
-    customers << "#{x}. #{i} "
+    customers << " #{x}. #{i}"
     x+= 1
     end
   end
